031f613e5d5a2c287f6c8d6cc9a30e7522af5bfb
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-09-19 22:04:13 -0300

    mudanças back

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
 $route = $_SERVER["REQUEST_URI"];
 $method = $_SERVER["REQUEST_METHOD"];
@@ -6,7 +8,14 @@ $method = $_SERVER["REQUEST_METHOD"];
 if($method == "GET"){
 
     if($route == "/usuarios"){
-        echo "Buscando os usuários disponíveis no sistema";
+        $json = [
+    
+            "email" => "ricardo@gmail.com",
+            "senha" => "12345"
+
+        ];
+        
+        echo json_encode($json);
     }
     
     else if($route == "/login"){
@@ -15,5 +24,23 @@ if($method == "GET"){
     else{ 
         echo "Não existe nenhuma rota nesse endereço!";
         return;
+    }
+}
+
+if($method == "POST"){
+    if($route == "/usuarios"){
+        $json_data = file_get_contents('php://input');
+        $data = json_decode($json_data, true);
+        $email = $data["email"];
+        $senha = $data["senha"];
+
+        $json_post = [
+            "Email" => $email,
+            "Senha" => $senha
+        ];
+
+        echo json_encode($json_post);
+            
+        
     }
 }
